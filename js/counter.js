b90828df331c0c9942f937bd67ad4cb64af7e4ef
Renan Rodrigues Ramos <renan.ramos598@gmail.com>
2018-06-20 16:40:51 -0300

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -27,7 +27,7 @@ function atualizaContador()
     minutos = (toString(mm).length) ? mm+' min e ' : '';  
     segundos = ss+' seg';
 
-    porcentagem = (dd / 280) * 100;
+    porcentagem = (1 - (dd / 280)) * 100;
     
     if (dd+hh+mm+ss > 0) 
     {
