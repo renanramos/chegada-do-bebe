5511dc0ef05e403293bf4142cf80710a4bb5ec7b
Renan Ramos <renanrramossi@gmail.com>
2018-07-23 08:53:29 -0300

    Retorno de informações para o modal, quando aberto.

--- js/dbMethods.js
+++ js/dbMethods.js
@@ -35,19 +35,26 @@ function writeMessages(){
 //lê as mensagens e adiciona na página
 function readMessages(){
 
-    ref.orderByChild('date').on("value", function(data){
+    ref.on("value", function(data){
+                
         let objKey = Object.keys(data.val());
+
         for(obj in objKey){
             let key = objKey[obj];
-            let a = document.getElementById("mensagem");
+            let mensagem = document.getElementById("mensagem");
+            let modalTitulo = document.getElementById("modalTitle");
+            let modalMessage =document.getElementById("modalMessage");
 
-            a.innerHTML += '<a href="#" class="list-group-item"><b>'+data.val()[key].name+' <span class="badge">'+data.val()[key].date+'</span></b></br>'+
-                                   data.val()[key].message+'</a></br>';
-
-            console.log(data.val()[key].name);
+            mensagem.innerHTML += '<a href="#" class="list-group-item" data-toggle="modal" data-target="#modal"><b>' + data.val()[key].name + ' <span class="badge">' + data.val()[key].date + '</span></b></br>' +
+                                    data.val()[key].message + '</a></br>';
+            
+            modalTitulo.innerHTML = data.val()[key].name + '  -  ' + data.val()[key].date;
+            modalMessage.innerHTML = data.val()[key].message;
+            
         }
+                
     }, function(errorObject){
-        a.innerHTML += '<a href="#"> A leitura do arquivo falhou. Codigo: '+errorObject.code+'</a>';
+        a.innerHTML += '<a href="#"> A leitura do arquivo falhou. Codigo: ' + errorObject.code + '</a>';
     });       
 }
 
