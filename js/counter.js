eb058e60f12e349cb8e6ec8f8a9fa39c28c9fadf
Renan Ramos <renanrramossi@gmail.com>
2018-10-15 13:37:20 -0300

    Data mais próxima para o nascimento.

--- js/counter.js
+++ js/counter.js
@@ -7,7 +7,7 @@
 function atualizaContador() 
 {  
     var hoje = new Date();    
-    var dataFim = new Date(2018, 11, 11);
+    var dataFim = new Date(2018, 11, 12);
     var dataPrev = new Date(dataFim - (30 *24 * 60 * 60 * 1000));
 
     var ss = parseInt((dataPrev - hoje) / 1000);
